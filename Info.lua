3cb4dbb1b680485a367dc5b293d44cf69608aae3
Niels <niels.breuker@outlook.com>
2024-06-10 11:29:32 +0200

    Cleaned up Info.lua description

--- Info.lua
+++ Info.lua
@@ -10,13 +10,13 @@ g_PluginInfo =
 	DisplayVersion = "1.1",
 	Date = "2024-06-10", -- yyyy-mm-dd
 	SourceLocation = "https://github.com/NiLSPACE/ChunkGen",
-	Description = [[Inspired by {%a https://github.com/cuberite/chunkworx}ChunkWorx{%/a} this plugin allows the user to easily (re)generate chunks from the webadmin. ChunkWorx still works, but it's been 10 years since anyone has looked at the code. There are lots of magic numbers, and the interface loses it's state everytime there is a reload. For this reason I made this plugin. Not just to try make a more user friendly replacement, but also to see how viable Ajax calls are when used inside Cuberite.
+	Description = [[Inspired by {%a https://github.com/cuberite/chunkworx}ChunkWorx{%/a} this plugin allows the user to easily (re)generate chunks from the webadmin. ChunkWorx still works, but it's been 10 years since anyone has looked at the code. There are lots of magic numbers which makes maintaining the code harder. The interface also loses it's state everytime there is a reload which makes development on the world generator more frustrating. This plugin tries to solve all these issues and add some more functionality to make world generator development easier.
     
     Some advantages over ChunkWorx:
     {%list}
-    {%li}Async interface{%/li}
-    {%li}Allow any radius around a player instead of just the exact chunk or 3x3{%/li}
-    {%li}Statistics about the generator, lighting en storage queues.{%/li}
+    {%li}Interface state isn't lost when Cuberite reloads.{%/li}
+    {%li}Allow any radius around a player instead of just the exact chunk or 3x3.{%/li}
+    {%li}Shows statistics about the generator, lighting and storage queues.{%/li}
     {%li}The to be generated chunks can spiral around a set of coordinates or a player which allows you to see the new chunks immediately instead of waiting for the updates to finally catch up to you.{%/li}
     {%/list}
 	]],
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-Inspired by [ChunkWorx](https://github.com/cuberite/chunkworx) this plugin allows the user to easily (re)generate chunks from the webadmin. ChunkWorx still works, but it's been 10 years since anyone has looked at the code. There are lots of magic numbers, and the interface loses it's state everytime there is a reload. For this reason I made this plugin. Not just to try make a more user friendly replacement, but also to see how viable Ajax calls are when used inside Cuberite.
+Inspired by [ChunkWorx](https://github.com/cuberite/chunkworx) this plugin allows the user to easily (re)generate chunks from the webadmin. ChunkWorx still works, but it's been 10 years since anyone has looked at the code. There are lots of magic numbers which makes maintaining the code harder. The interface also loses it's state everytime there is a reload which makes development on the world generator more frustrating. This plugin tries to solve all these issues and add some more functionality to make world generator development easier.
 
 Some advantages over ChunkWorx: 
  
- - Async interface 
- - Allow any radius around a player instead of just the exact chunk or 3x3 
- - Statistics about the generator, lighting en storage queues. 
+ - Interface state isn't lost when Cuberite reloads. 
+ - Allow any radius around a player instead of just the exact chunk or 3x3. 
+ - Shows statistics about the generator, lighting and storage queues. 
  - The to be generated chunks can spiral around a set of coordinates or a player which allows you to see the new chunks immediately instead of waiting for the updates to finally catch up to you.
 
 # Commands
